1a2915d3f894b89540ae68938efdb8bdccf442e2
dannycrews <dancrews@me.com>
2016-08-08 11:46:40 -0400

    Move models into modules, Add forms to add bands and songs

--- app/routes/bands.js
+++ app/routes/bands.js
@@ -1,24 +1,14 @@
 import Ember from 'ember';
+import Band from '../models/band';
+import Song from '../models/song';
 
-var Band = Ember.Object.extend({
-    name: '',
-
-    slug: Ember.computed('name', function () {
-        return this.get('name').dasherize();
-    }),
-});
-
-var Song = Ember.Object.extend({
-    title: '',
-    rating: 0,
-    band: ''
-});
 
 export default Ember.Route.extend({
-    model: function () {
+    model: function() {
         var blackDog = Song.create({
             title: 'Black Dog',
-            band: 'Led Zeppelin', rating: 3
+            band: 'Led Zeppelin',
+            rating: 3
         });
         var yellowLedbetter = Song.create({
             title: 'Yellow Ledbetter',
@@ -26,20 +16,37 @@ export default Ember.Route.extend({
             rating: 4
         });
         var pretender = Song.create({
-            title: 'The Pretender', band: 'Foo Fighters', rating: 2
+            title: 'The Pretender',
+            band: 'Foo Fighters',
+            rating: 2
         });
         var daughter = Song.create({
             title: 'Daughter',
-            band: 'Pearl Jam', rating: 5
+            band: 'Pearl Jam',
+            rating: 5
         });
 
         var ledZeppelin = Band.create({
-            name: 'Led Zeppelin', songs: [blackDog] });
+            name: 'Led Zeppelin',
+            songs: [blackDog]
+        });
         var pearlJam = Band.create({
-            name: 'Pearl Jam', songs: [yellowLedbetter, daughter] });
+            name: 'Pearl Jam',
+            songs: [yellowLedbetter, daughter]
+        });
         var fooFighters = Band.create({
-            name: 'Foo Fighters', songs: [pretender] });
-            
-            return [ledZeppelin, pearlJam, fooFighters];
+            name: 'Foo Fighters',
+            songs: [pretender]
+        });
+
+        return [ledZeppelin, pearlJam, fooFighters];
+    },
+    actions: {
+        createBand: function() {
+            var name = this.get('controller').get('name');
+            var band = Band.create({ name: name });
+            this.modelFor('bands').pushObject(band);
+            this.get('controller').set('name', '');
         }
+    }
 });
--- app/routes/bands/songs.js
+++
@@ -1,7 +0,0 @@
-import Ember from 'ember';
-
-export default Ember.route.extend({
-    model: function() {
-        return this.modelFor('bands.band');
-    }
-});
