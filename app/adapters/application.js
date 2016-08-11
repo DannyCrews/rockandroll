542a08e8b1421cb88f0fb653ad4bbc7544220b48
dannycrews <dancrews@me.com>
2016-08-11 16:52:14 -0400

    Move from slug to id

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -3,4 +3,8 @@ import ENV from '../config/environment';
 
 export default DS.JSONAPIAdapter.extend({
   host: ENV.apiHost
+
+  shouldBackgroundReloadRecord: function() {
+    return false;
+  }
 });
