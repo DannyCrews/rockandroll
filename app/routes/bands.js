bdbb3a304b727c955802507dd0a6e4a017a6ac6f
dannycrews <dancrews@me.com>
2016-08-11 16:33:43 -0400

    Connect to API. Define application adapter and set up in config.

--- app/routes/bands.js
+++ app/routes/bands.js
@@ -1,47 +1,12 @@
 import Ember from 'ember';
 import Band from '../models/band';
-import Song from '../models/song';
 
 
 export default Ember.Route.extend({
     model: function() {
-        var blackDog = Song.create({
-            title: 'Black Dog',
-            band: 'Led Zeppelin',
-            rating: 3
-        });
-        var yellowLedbetter = Song.create({
-            title: 'Yellow Ledbetter',
-            band: 'Pearl Jam',
-            rating: 4
-        });
-        var pretender = Song.create({
-            title: 'The Pretender',
-            band: 'Foo Fighters',
-            rating: 2
-        });
-        var daughter = Song.create({
-            title: 'Daughter',
-            band: 'Pearl Jam',
-            rating: 5
-        });
-
-        var ledZeppelin = Band.create({
-            name: 'Led Zeppelin',
-            songs: [blackDog]
-        });
-        var pearlJam = Band.create({
-            name: 'Pearl Jam',
-            description: 'Pearl Jam is an American rock band, formed in Seattle, Washington in 1990.',
-            songs: [yellowLedbetter, daughter]
-        });
-        var fooFighters = Band.create({
-            name: 'Foo Fighters',
-            songs: [pretender]
-        });
-
-        return [ledZeppelin, pearlJam, fooFighters];
+        return this.store.findAll('band');
     },
+
     actions: {
         didTransition: function() {
           document.title = 'Bands - Rock & Roll';
