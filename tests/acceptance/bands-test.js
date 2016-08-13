ee22dc7a97b14c28dd4b6b4ec76245a9af01facc
dannycrews <dancrews@me.com>
2016-08-13 07:28:03 -0400

    add test for creating a song in two steps

--- tests/acceptance/bands-test.js
+++ tests/acceptance/bands-test.js
@@ -39,47 +39,99 @@ test('List bands', function(assert) {
       });
     });
 
-      test('List bands', function(assert) {
-        server = new Pretender(function() {
-          this.get('/bands', function() {
-           var response = {
-            data: [
-            {
-              id: 1,
-              type: 'bands',
-              attributes: {
-                name: 'Radiohead'
-              }
+test('List bands', function(assert) {
+  server = new Pretender(function() {
+    this.get('/bands', function() {
+     var response = {
+      data: [
+      {
+        id: 1,
+        type: 'bands',
+        attributes: {
+          name: 'Radiohead'
+        }
+      }
+      ]
+    };
+    return [200, { 'Content-Type': 'application/vnd.api+json' }, JSON.stringify(response)];
+  });
+
+    this.post('/bands', function() { var response = {
+      data: [
+       {
+        id: 2,
+        type: 'bands',
+        attributes: {
+          name: 'Long Distance Calling'
+        }
+      }
+      ]
+    };
+    return [200, { 'Content-Type': 'application/vnd.api+json' },
+    JSON.stringify(response)];
+  });
+  });
+
+  visit('/bands');
+
+  fillIn('.new-band', 'Long Distance Calling');
+  click('.new-band-button');
+  andThen(function() {
+    assert.equal(find('.band-link').length, 2, 'All band links are rendered');
+    assert.equal(find('.band-link:last').text().trim(), 'Long Distance Calling',
+     'Created band appears at the end of the list');
+    assert.equal(find('.nav a.active:contains("Songs")').length, 1,
+      'The Songs tab is active');
+  });
+});
+
+test('Create a song in two steps', function(assert) {
+  server = new Pretender(function() {
+    this.get('/bands', function() {
+      var response = {
+        data: [
+          {
+            id: 1,
+            type: 'bands',
+            attributes: {
+              name: 'Radiohead'
             }
-            ]
-          };
-          return [200, { 'Content-Type': 'application/vnd.api+json' }, JSON.stringify(response)];
-        });
-
-          this.post('/bands', function() { var response = {
-            data: [ {
-              id: 2,
-              type: 'bands',
-              attributes: {
-                name: 'Long Distance Calling'
-              }
+          }
+        ]
+      };
+      return [200, { 'Content-Type': 'application/vnd.api+json' },
+      JSON.stringify(response)];
+    });
+    this.post('/songs', function() {
+      var response = {
+        data: [
+          {
+            id: 1,
+            type: 'songs',
+            attributes: {
+              name: 'Killer Cars'
             }
-            ]
-          };
-          return [200, { 'Content-Type': 'application/vnd.api+json' },
-          JSON.stringify(response)];
-        });
-        });
+          }
+        ]
+      };
+      return [200, { 'Content-Type': 'application/vnd.api+json' },
+       JSON.stringify(response)];
+    });
 
-        visit('/bands');
+    this.get('/bands/1/songs', () => {
+      return [200, { 'Content-Type': 'application/vnd.api+json' },
+        JSON.stringify({ data: [] })];
+    });
+  });
 
-        fillIn('.new-band', 'Long Distance Calling');
-        click('.new-band-button');
-        andThen(function() {
-          assert.equal(find('.band-link').length, 2, 'All band links are rendered');
-          assert.equal(find('.band-link:last').text().trim(), 'Long Distance Calling',
-           'Created band appears at the end of the list');
-          assert.equal(find('.nav a.active:contains("Songs")').length, 1,
-            'The Songs tab is active');
-        });
-      });
+  visit('/');
+  click('.band-link:contains("Radiohead")');
+  click('a:contains("create one")');
+  fillIn('.new-song', 'Killer Cars');
+  triggerEvent('.new-song-form', 'submit');
+
+  andThen(function() {
+    assert.equal(find('.songs .song:contains("Killer Cars")').length,
+      1, "Creates the song and displays it in the list");
+  });
+});
