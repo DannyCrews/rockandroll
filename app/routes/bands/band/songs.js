49a8e1e54c5dd2cc2fb006511705090a93c6abd3
dannycrews <dancrews@me.com>
2016-08-13 16:26:54 -0400

    Add capitalization template helper

--- app/routes/bands/band/songs.js
+++ app/routes/bands/band/songs.js
@@ -1,5 +1,6 @@
 import Ember from 'ember';
 // import wait from '../../../utils/wait';
+import { capitalize as capitalizeWords } from '../../../helpers/capitalize';
 
 export default Ember.Route.extend({
   model: function() {
@@ -13,7 +14,8 @@ export default Ember.Route.extend({
   actions: {
     didTransition: function() {
       var band = this.modelFor('bands.band');
-      document.title = `${band.get('name')} songs - Rock & Roll`;
+      var name = capitalizeWords(band.get('name'));
+      document.title = `${name} songs - Rock & Roll`;
     },
 
     createSong: function() {
--- tests/acceptance/bands-test.js
+++ tests/acceptance/bands-test.js
@@ -193,7 +193,7 @@ click('button.sort-title-desc');
 
 andThen(function() {
   assert.equal(currentURL(), '/bands/1/songs?sort=titleDesc');
-  assertTrimmedText(assert, '.song:first', 'Spinning in Daffodils',
+  assertTrimmedText(assert, '.song:first', 'Spinning In Daffodils',
   'The first song is the one that is the last in the alphabet');
 
   assertTrimmedText(assert, '.song:last', 'Elephants',
@@ -206,7 +206,7 @@ andThen(function() {
   assert.equal(currentURL(), '/bands/1/songs?sort=ratingAsc');
   assertTrimmedText(assert, '.song:first', 'Mind Eraser, No Chaser',
   'The first song is the lowest ranked, first in the alphabet');
-      assertTrimmedText(assert, '.song:last', 'Spinning in Daffodils',
+      assertTrimmedText(assert, '.song:last', 'Spinning In Daffodils',
   'The last song is the highest ranked, last in the alphabet');
   });
 });
